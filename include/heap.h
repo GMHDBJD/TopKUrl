201817abd1c980f1aacf58ee46caf6cc5b5c583f
gmhdbjd <gmhdbjd@gmail.com>
2020-03-20 10:30:47 -0400

    update heap

--- include/heap.h
+++ include/heap.h
@@ -16,7 +16,7 @@ using std::vector;
 class Heap
 {
 private:
-    vector<pair<string, size_t>> data_;
+    vector<pair<size_t, string>> data_;
     map<string, size_t> hashtable_;
 
     void fixHeap(size_t pos)
@@ -27,13 +27,13 @@ private:
             size_t left_pos = pos * 2 + 1;
             size_t right_pos = left_pos + 1;
             size_t next_pos = pos;
-            if (left_pos < data_.size() && data_[left_pos].second < data_[next_pos].second)
+            if (left_pos < data_.size() && data_[left_pos] < data_[next_pos])
                 next_pos = left_pos;
-            if (right_pos < data_.size() && data_[right_pos].second < data_[next_pos].second)
+            if (right_pos < data_.size() && data_[right_pos] < data_[next_pos])
                 next_pos = right_pos;
             if (next_pos != pos)
             {
-                swap(hashtable_[data_[pos].first], hashtable_[data_[next_pos].first]);
+                swap(hashtable_[data_[pos].second], hashtable_[data_[next_pos].second]);
                 swap(data_[pos], data_[next_pos]);
                 return fixHeap(next_pos);
             }
@@ -41,15 +41,15 @@ private:
     }
 
 public:
-    void push(const pair<string, size_t> &p)
+    void push(const pair<size_t, string> &p)
     {
         data_.push_back(p);
         size_t pos = data_.size() - 1;
-        hashtable_[p.first] = pos;
+        hashtable_[p.second] = pos;
         size_t parent_pos = (pos + 1) / 2 - 1;
-        while (pos > 0 && data_[parent_pos].second > data_[pos].second)
+        while (pos > 0 && data_[parent_pos] > data_[pos])
         {
-            swap(hashtable_[data_[pos].first], hashtable_[data_[parent_pos].first]);
+            swap(hashtable_[data_[pos].second], hashtable_[data_[parent_pos].second]);
             swap(data_[pos], data_[parent_pos]);
             pos = parent_pos;
             parent_pos = (pos + 1) / 2 - 1;
@@ -61,7 +61,7 @@ public:
         auto iter = hashtable_.find(key);
         if (iter != hashtable_.end())
         {
-            ++data_[iter->second].second;
+            ++data_[iter->second].first;
             fixHeap(iter->second);
             return true;
         }
@@ -76,13 +76,13 @@ public:
         return data_.size();
     }
 
-    pair<string, size_t> pop()
+    pair<size_t, string> pop()
     {
         auto front = data_.front();
         auto back = data_.back();
         data_[0] = back;
-        hashtable_[back.first] = 0;
-        hashtable_.erase(front.first);
+        hashtable_[back.second] = 0;
+        hashtable_.erase(front.second);
         data_.pop_back();
         fixHeap(0);
         return front;
@@ -93,11 +93,10 @@ public:
         return data_.empty();
     }
 
-    const pair<string, size_t> &front() const
+    const pair<size_t, string> &front() const
     {
         return data_.front();
     }
-
 };
 
 #endif
